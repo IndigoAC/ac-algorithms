89470e42290787c9cefda1d67f57e2e88dd5ac4b
Indigo <Indigo@anniecannons.com>
2019-09-16 12:03:38 -0700

    solution to cyclic rotation

--- cyclicRotation.js
+++ cyclicRotation.js
@@ -13,7 +13,19 @@
 //that, given a zero-indexed array A consisting of N integers and an integer K, returns the array A rotated K times.
 //
 //    For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].\
+//arr =Array
+
+//Input: I have 2 inputs, an array and a number
+//Output: an array (with the numbers rearranged)
+// Tools: loop (use loops because we are running something a certain amount of times) - the loops will run for the number of rotations that we wnat the array to go through cilclicRotation ([2, 10,30,56,],3)         cilclicRotation ([2, 10,30,56,],30).....
+//shift, unshift, push and pop methods to work with arrays, we want to use them to get the last element in the array and move it to the beginning each time the loops runs 
+//Output the array once the loops has finished
 
 function cyclicRotation(arr, rotation){
-    //Your code here...
-}
+    for(var i = 0; i < rotation; i++){
+      var moveIt = arr.pop();
+      arr.unshift(moveIt);
+    }
+   return arr;
+  } 
+       console.log(cyclicRotation([7,50,0,9,],55))
