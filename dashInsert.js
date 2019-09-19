2f38d6c30a1bed9364741ae53e1fcd7bc06e1de8
Indigo <Indigo@anniecannons.com>
2019-09-19 11:36:38 -0700

    my complete dashInsert

--- dashInsert.js
+++ dashInsert.js
@@ -24,7 +24,28 @@ var result = isBigger("13");
 console.log(result) // "1-3"
 
 **/
+// Input: a number
+// Output: a modified string from the input with dashes in between the odd numbers of that string
+// Step 1: Create a variable to store the string
+// Step 2: Create a variable to store the transformation of the number t an array of numbers, by first turning into first, a string, and then to an array Tools: .toString().split("")
+// 4982
+// ["4", "9", "8", "2"]
+// Step 3: Create a for loop to go through each of the numbers in the array
+    //Need a conditional statement that will test if the number is odd && if the number after it is odd
+        // If true, I want to add the number and the dash to the string
+        // If false, I want to just add the number
 
-function dashInsert(str) {
-	// Your code
+function dashInsert(num) {
+	var output = "";
+	var array = num.toString().split("");
+	for (var i = 0; i < array.length; i++){
+		if ( array[i]% 2 === 1 && array [i+ 1]% 2 === 1) // Every time the loop run it changes the value you of i ["4","2","3"]
+		{
+			output = output + array[i] + "-";
+		} else {
+			ouput = output + array[i];
+		}
+	}
+	return output;
 }
+dashInsert (56780)
